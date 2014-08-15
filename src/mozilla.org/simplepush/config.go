93775f59f4f4869857a0d7ce18315468e80ba9a3
Ben Bangert <ben@groovie.org>
2014-08-15 13:53:35 -0700

    Style cleanups, doc additions.

--- src/mozilla.org/simplepush/config.go
+++ src/mozilla.org/simplepush/config.go
@@ -13,32 +13,37 @@ import (
 	"github.com/bbangert/toml"
 )
 
-// Extensible sections
+// A mapping type used to represent the available extensions that can be
+// loaded for a given config section.
 type AvailableExtensions map[string]func() HasConfigStruct
 
 type ExtensibleGlobals struct {
 	Typ string `toml:"type"`
 }
 
-// Generic config section with any set of options
+// Generic config section with any set of options.
 type ConfigFile map[string]toml.Primitive
 
-// Interface for something that needs a set of config options
+// Interface for something that needs a set of config options, and how it
+// should be initialized with the options.
 type HasConfigStruct interface {
 	// Returns a default-value-populated configuration structure into which
 	// the plugin's TOML configuration will be deserialized.
 	ConfigStruct() interface{}
-	// The configuration loaded after ConfigStruct will be passed in
-	// Throwing an error here will cause the application to stop loading
+	// The configuration loaded after ConfigStruct will be passed in.
+	// Throwing an error here will cause the application to stop loading.
 	Init(app *Application, config interface{}) error
 }
 
 var unknownOptionRegex = regexp.MustCompile("^Configuration contains key \\[(?P<key>\\S+)\\]")
 
-func LoadConfigStruct(config toml.Primitive, configable HasConfigStruct) (
-	configStruct interface{}, err error) {
-
-	configStruct = configable.ConfigStruct()
+// Load a config section using a configable and return the resulting loaded
+// config object.
+func LoadConfigStruct(
+	config toml.Primitive,
+	configable HasConfigStruct,
+) (interface{}, error) {
+	configStruct := configable.ConfigStruct()
 
 	// Global section options
 	// SimplePush defines some common parameters
@@ -57,7 +62,8 @@ func LoadConfigStruct(config toml.Primitive, configable HasConfigStruct) (
 		spParams[kname] = true
 	}
 
-	if err = toml.PrimitiveDecodeStrict(config, configStruct, spParams); err != nil {
+	err := toml.PrimitiveDecodeStrict(config, configStruct, spParams)
+	if err != nil {
 		configStruct = nil
 		matches := unknownOptionRegex.FindStringSubmatch(err.Error())
 		if len(matches) == 2 {
@@ -65,30 +71,39 @@ func LoadConfigStruct(config toml.Primitive, configable HasConfigStruct) (
 			err = fmt.Errorf("Unknown config setting: %s", matches[1])
 		}
 	}
-	return
+	return configStruct, err
 }
 
-// Loads the config for a section supplied, configures the supplied object, and initializes
-func LoadConfigForSection(app *Application, sectionName string, obj HasConfigStruct,
-	configFile ConfigFile) (err error) {
-
+// Loads the config for a section supplied, configures the supplied object, and
+// initializes it.
+func LoadConfigForSection(
+	app *Application,
+	sectionName string,
+	obj HasConfigStruct,
+	configFile ConfigFile,
+) error {
 	conf, ok := configFile[sectionName]
 	if !ok {
 		return fmt.Errorf("Error loading config file, section: %s", sectionName)
 	}
 	confStruct := obj.ConfigStruct()
-	if err = toml.PrimitiveDecode(conf, confStruct); err != nil {
-		err = fmt.Errorf("Unable to decode config for section '%s': %s",
+	err := toml.PrimitiveDecode(conf, confStruct)
+	if err != nil {
+		return fmt.Errorf("Unable to decode config for section '%s': %s",
 			sectionName, err)
-		return
 	}
 	err = obj.Init(app, confStruct)
-	return
+	return err
 }
 
-// Load an extensible section that has a type keyword
-func LoadExtensibleSection(app *Application, sectionName string,
-	extensions AvailableExtensions, configFile ConfigFile) (interface{}, error) {
+// Load an extensible section that has a type keyword using one of the available
+// supplied extensions.
+func LoadExtensibleSection(
+	app *Application,
+	sectionName string,
+	extensions AvailableExtensions,
+	configFile ConfigFile,
+) (interface{}, error) {
 	var err error
 
 	confSection := new(ExtensibleGlobals)
@@ -118,13 +133,13 @@ func LoadExtensibleSection(app *Application, sectionName string,
 }
 
 // Handles reading a TOML based configuration file, and loading an
-// initialized Application, ready to Run
-func LoadApplicationFromFileName(filename string) (app *Application, err error) {
+// initialized Application that is ready to Run.
+func LoadApplicationFromFileName(filename string) (*Application, error) {
 	var (
-		obj        interface{}
 		configFile ConfigFile
+		err        error
+		obj        interface{}
 	)
-
 	if _, err = toml.DecodeFile(filename, &configFile); err != nil {
 		return nil, fmt.Errorf("Error decoding config file: %s", err)
 	}
@@ -133,52 +148,52 @@ func LoadApplicationFromFileName(filename string) (app *Application, err error) 
 	// available on the Application at each stage of application setup
 
 	// Setup the base application first
-	app = new(Application)
+	app := new(Application)
 	if err = LoadConfigForSection(nil, "default", app, configFile); err != nil {
-		return
+		return nil, err
 	}
 
 	// Next, many things require the logger, and logger has no other deps
 	if obj, err = LoadExtensibleSection(app, "logging", AvailableLoggers, configFile); err != nil {
-		return
+		return nil, err
 	}
 	logger, _ := obj.(Logger)
 	if err = app.SetLogger(logger); err != nil {
-		return
+		return nil, err
 	}
 
 	// Next, metrics, Deps: Logger
 	metrics := new(Metrics)
 	if err = LoadConfigForSection(app, "metrics", metrics, configFile); err != nil {
-		return
+		return nil, err
 	}
 	if err = app.SetMetrics(metrics); err != nil {
-		return
+		return nil, err
 	}
 
 	// Next, storage, Deps: Logger, Metrics
 	storage := new(Storage)
 	if err = LoadConfigForSection(app, "storage", storage, configFile); err != nil {
-		return
+		return nil, err
 	}
 	if err = app.SetStorage(storage); err != nil {
-		return
+		return nil, err
 	}
 
 	// Next, setup the router, Deps: Logger, Metrics
 	router := new(Router)
 	if err = LoadConfigForSection(app, "router", router, configFile); err != nil {
-		return
+		return nil, err
 	}
 	if err = app.SetRouter(router); err != nil {
-		return
+		return nil, err
 	}
 
 	// Finally, setup the handlers, Deps: Logger, Metrics
 	serv := new(Serv)
 	configStruct := serv.ConfigStruct()
 	if err = toml.PrimitiveDecode(configFile["default"], configStruct); err != nil {
-		return
+		return nil, err
 	}
 	serv.Init(app, configStruct)
 	app.SetServer(serv)
@@ -186,5 +201,5 @@ func LoadApplicationFromFileName(filename string) (app *Application, err error) 
 	handlers := new(Handler)
 	handlers.Init(app, nil)
 	app.SetHandlers(handlers)
-	return
+	return app, nil
 }
